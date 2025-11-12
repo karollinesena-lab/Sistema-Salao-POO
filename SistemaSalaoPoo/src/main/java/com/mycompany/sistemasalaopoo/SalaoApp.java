1a0f37c4e7d0f1381de126841084891539997bfb
karollinesena <karollinedesena@gmail.com>
2025-11-11 21:38:05 -0300

    Organização dos arquivos

---
+++ SistemaSalaoPoo/src/main/java/com/mycompany/sistemasalaopoo/SalaoApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemasalaopoo;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+/**
+ *
+ * @author Karolline Sena
+ * 
+ */
+public class SalaoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
